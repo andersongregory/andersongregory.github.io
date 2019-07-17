f8b12d7dfa862ea85731f2a8e6ee7175e2e53de7
andersongregory <48030567+andersongregory@users.noreply.github.com>
2019-07-17 16:53:44 -0300

    Add files via upload

---
+++ aulaJS/js/meuJavasccript.js
@@ -0,0 +1,20 @@
+function myFunction() {
+   if (document.getElementById("demo").innerText.length === 0 ){
+
+    window.alert("Este é um alerta!");
+    console.log("Só apareço com o F12");
+
+function minhaFunção2() {
+    document.getElementById("demo1").innerHTML = 
+    "Olá Dollinho!";
+    
+    document.getElementById("demo2").innerHTML = 
+    "O melhor do Brasil. O melhor!";
+ }else{
+     document.getElementById("demo1").innerHTML = "";
+
+     document.getElementById("demo2").innerHTML = "";
+     document.querySelector("#divCentral").style
+}
+
+
